714dd6b7083e25cb2dc7de37262f67c752197fcf
Johnotalvaro <johnotalvaro07@gmail.com>
2023-04-19 19:38:48 -0500

    Creacion del metodo que me permite insertar un teacher en la base de datos

--- src/assets/js/teachers/repository.js
+++ src/assets/js/teachers/repository.js
@@ -1,1 +1,11 @@
 // Guardar, Actualizar, Leer o eliminar los datos en el storage
+import { getDatabase, setDatabase } from './../utils/storage';
+
+const dbName = 'db_teachers';
+
+export function createTeacher(teacher) {
+    const arrayTeachers = getDatabase(dbName);
+    arrayTeachers.push(teacher);
+    setDatabase(dbName, arrayTeachers);
+}
+
