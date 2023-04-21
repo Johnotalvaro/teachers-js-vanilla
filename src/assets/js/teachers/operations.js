a50b22e36a554ab4c8d7829776e55e228b08c83c
Johnotalvaro <johnotalvaro07@gmail.com>
2023-04-20 20:55:09 -0500

    Se obtienen para cada teacher un id unico, con la funcion getTime de las fechas, ademas se crea la plantilla para cuando la tabla esta vacia

--- src/assets/js/teachers/operations.js
+++ src/assets/js/teachers/operations.js
@@ -22,38 +22,81 @@ function listTeachers() {
     const tbody = document.querySelector('#tblTeachers tbody');
     tbody.innerHTML = '';
 
-    arrayTeachers.forEach( (teacher, index)  => {
-        // Creo la fila
-        const row = document.createElement('tr');
+    if (arrayTeachers.length > 0) {
 
-        // Creo las columnas
-        const colId = document.createElement('td');
-        colId.textContent = index;
+        arrayTeachers.forEach((teacher) => {
 
-        const colName = document.createElement('td');
-        colName.textContent = teacher.name;
+            const { id, name, description, email, birthDate } = teacher;
 
-        const colDescription = document.createElement('td');
-        colDescription.textContent = teacher.description;
+            // Creo la fila
+            const row = document.createElement('tr');
+            row.classList.add('align-middle')
 
-        const colEmail = document.createElement('td');
-        colEmail.textContent = teacher.email;
+            // Creo las columnas
+            const colId = document.createElement('td');
+            colId.textContent = id;
+            colId.classList.add('text-center')
 
-        const colBirthDate = document.createElement('td');
-        colBirthDate.textContent = teacher.birthDate;
+            const colName = document.createElement('td');
+            colName.textContent = name;
 
-        const colButtons = document.createElement('td');
-        colButtons.textContent = teacher.Buttons;
+            const colDescription = document.createElement('td');
+            colDescription.textContent = description;
 
-        // Agrego las columnas a la fila
-        row.appendChild(colId);
-        row.appendChild(colName);
-        row.appendChild(colDescription);
-        row.appendChild(colEmail);
-        row.appendChild(colBirthDate);
-        row.appendChild(colButtons);
+            const colEmail = document.createElement('td');
+            colEmail.textContent = email;
 
-        // Agrego la fila al tbody
-        tbody.appendChild(row);
-    });
+            const colBirthDate = document.createElement('td');
+            colBirthDate.textContent = birthDate;
+
+            const colButtons = document.createElement('td');
+            colButtons.classList.add('text-center');
+
+            const editButton = document.createElement('button');
+            editButton.classList.add('btn', 'btn-primary', 'btn-edit', 'm-1');
+            editButton.dataset.id = id;
+            editButton.setAttribute('title', 'Editar');
+
+            const editIcon = document.createElement('em');
+            editIcon.classList.add('fa', 'fa-pencil');
+            editButton.appendChild(editIcon);
+
+            colButtons.appendChild(editButton);
+
+
+            const deleteButton = document.createElement('button');
+            deleteButton.classList.add('btn', 'btn-danger', 'btn-delete', 'm-1');
+            deleteButton.dataset.id = id;
+            deleteButton.setAttribute('title', 'Eliminar');
+
+            const deleteIcon = document.createElement('em');
+            deleteIcon.classList.add('fa', 'fa-trash');
+            deleteButton.appendChild(deleteIcon);
+
+            colButtons.appendChild(deleteButton);
+
+            // Agrego las columnas a la fila
+            row.appendChild(colId);
+            row.appendChild(colName);
+            row.appendChild(colDescription);
+            row.appendChild(colEmail);
+            row.appendChild(colBirthDate);
+            row.appendChild(colButtons);
+
+            // Agrego la fila al tbody
+            tbody.appendChild(row);
+        });
+
+    } else {
+
+        const rowEmpty = document.createElement('tr');
+        const colEmpty = document.createElement('td');
+        colEmpty.setAttribute('colSpan', '6');
+        colEmpty.textContent = "No se encuentran registros disponibles";
+        colEmpty.classList.add('text-center');
+        rowEmpty.appendChild(colEmpty);
+
+        tbody.appendChild(rowEmpty);
+        
+    }
 }
